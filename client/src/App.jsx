c7bbf1c444f05cd56a2199171daafcc5f016ce22
Louis Qian <yuq4@illinois.edu>
2022-03-08 21:07:23 -0600

    Inital Commit

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import { Navbar, Welcome, Services, Footer, Transaction } from "./components";
+
+const App = () => {
+  return (
+    <div className="min-h-screen">
+      <div className="gradient-bg-welcome">
+        <Navbar />
+        <Welcome />
+      </div>
+      <Services />
+      <Transaction />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
